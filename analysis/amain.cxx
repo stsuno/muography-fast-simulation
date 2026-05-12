2b35226c45ffd48b16a9bb321d4331cf5ab10ee5
stsuno <soshi.tsuno@gmail.com>
2026-05-12 09:40:29 +0900

    update

--- analysis/amain.cxx
+++ analysis/amain.cxx
@@ -18,41 +18,58 @@ int main(int argc, char **argv) {
   //================
   // Input & Output
   //================
-  TString FDirectoryName;
-  char *inputfilename = argv[1];
-  std::cout << "Input card name : " << inputfilename << std::endl;
 
-  TString FRootFileName;
+  int imode = std::stoi(argv[1]);
 
-  Int_t totFileNumber = -2;
-  std::ifstream dataTmp(inputfilename);
-  while (!dataTmp.eof()) {
-    char stmp[1000];
-    dataTmp >> stmp;
-    totFileNumber++;
+  std::string FDirectoryName = argv[2];
+  std::string inputfilename1 = argv[3];
+  std::string inputfilename2;
+
+  if (imode<1 || imode>2) {
+    std::cout << "ERROR::Set mode=1 or 2." << std::endl;
+    abort();
   }
 
-  std::ifstream data(inputfilename);
-  data >> FDirectoryName;
+  std::cout << "Input filename 1 : " << inputfilename1 << std::endl;
+  if (imode==2) {
+    inputfilename2=argv[4]; 
+    std::cout << "Input filename 2 : " << inputfilename2 << std::endl;
+  }
+
+  TVector3 MWPC_position(0.0,0.0,0.0);;
+  if (inputfilename1.find("doubleM1B")!=std::string::npos) { MWPC_position.SetXYZ(0.0,   0.0,2400.0); }
+  if (inputfilename1.find("doubleM2B")!=std::string::npos) { MWPC_position.SetXYZ(0.0, 100.0,2400.0); }
+  if (inputfilename1.find("doubleM3B")!=std::string::npos) { MWPC_position.SetXYZ(0.0,-100.0,2400.0); }
+  if (inputfilename1.find("doubleM4B")!=std::string::npos) { MWPC_position.SetXYZ(0.0, 200.0,2400.0); }
+  if (inputfilename1.find("doubleM5B")!=std::string::npos) { MWPC_position.SetXYZ(0.0,-200.0,2400.0); }
+
+  if (inputfilename1.find("doubleM1C")!=std::string::npos) { MWPC_position.SetXYZ(1000.0,   0.0,2200.0); }
+  if (inputfilename1.find("doubleM2C")!=std::string::npos) { MWPC_position.SetXYZ(1000.0, 100.0,2200.0); }
+  if (inputfilename1.find("doubleM3C")!=std::string::npos) { MWPC_position.SetXYZ(1000.0,-100.0,2200.0); }
+  if (inputfilename1.find("doubleM4C")!=std::string::npos) { MWPC_position.SetXYZ(1000.0, 200.0,2200.0); }
+  if (inputfilename1.find("doubleM5C")!=std::string::npos) { MWPC_position.SetXYZ(1000.0,-200.0,2200.0); }
+
+  if (inputfilename1.find("doubleM1A")!=std::string::npos) { MWPC_position.SetXYZ(2000.0,   0.0,2400.0); }
+  if (inputfilename1.find("doubleM2A")!=std::string::npos) { MWPC_position.SetXYZ(2000.0, 100.0,2400.0); }
+  if (inputfilename1.find("doubleM3A")!=std::string::npos) { MWPC_position.SetXYZ(2000.0,-100.0,2400.0); }
+  if (inputfilename1.find("doubleM4A")!=std::string::npos) { MWPC_position.SetXYZ(2000.0, 200.0,2400.0); }
+  if (inputfilename1.find("doubleM5A")!=std::string::npos) { MWPC_position.SetXYZ(2000.0,-200.0,2400.0); }
+
   TChain* tree = new TChain("track_tree");
+  TString rin = FDirectoryName + inputfilename1 + "/track_tree";
+  tree->AddFile(rin,-1); 
 
-  char s[1000];
-  while (!data.eof()) {
-    data >> s;
-    TString rin = FDirectoryName + s + "/track_tree";
-    TString rinA = FDirectoryName + s;
-    if (!data.eof()) {
-      std::cout << "Input File name  : " << s << std::endl;
-      tree->AddFile(rin,-1); 
-    }
-  }
-
-  FRootFileName = s;
-  TString rout = "hist_" + FRootFileName;
+  TString rout = "hist_" + inputfilename1;
   std::cout << "Output File name : " << rout << std::endl;
 
   KomenAnalysis myana(tree);
   myana.initialize(rout);
+  myana.SetMode(imode);
+  if (imode==2) {
+    myana.SetDirectryName(FDirectoryName);
+    myana.SetCheckFile(inputfilename2);
+    myana.SetMWPCPosition(MWPC_position);
+  }
   myana.execute();
   myana.finalize();
 
